7f5f69362bb98d30c5dca6342f09848c28f66c31
jerewang2 <jwang3320@gmail.com>
2021-11-03 19:17:01 -0400

    Slightly Optimize New Year Chaos Code

--- New_Year_Chaos.py
+++ New_Year_Chaos.py
@@ -1,51 +1,26 @@
-# t = number of cases
-# n = length of list
-# q = list of sequence
-
-# t = 2, n = 5, q = [2, 1, 5, 3, 4]
-# q = [2, 1, 5, 3, 4]
-# print("Correct Answer: 3")
-
-# q = [2, 5, 1, 3, 4]
-# print("Correct Answer: Too Chaotic")
-
-# q = [5, 1, 2, 3, 7, 8, 6, 4]
-# print("Correct Answer: Too Chaotic")
-
-# q = [1, 2, 5, 3, 7, 8, 6, 4]
-# print("Correct Answer: 4")
-
-# q = [1, 2, 5, 3, 4, 7, 8, 6]
-# print("Correct Answer: 4")
-
-
-def swap(lineorder, num):
-    currentpos = lineorder.index(num)
-    nextpos = currentpos - 1
-    tempnextnum = lineorder[nextpos]
-    lineorder[nextpos] = num
-    lineorder[currentpos] = tempnextnum
-
-
 def minimumbribes(q):
-    lineorder = []
+    lineorder = list(range(1, len(q)+1))
     count = 0
     flag = 1
-    for i in range(len(q)):
-        lineorder.append(i+1)
 
     for pos in range(len(q)):
+        temporder = lineorder[:]
         if lineorder[pos] != q[pos]:
             orderdifference = abs(pos - lineorder.index(q[pos]))
             if orderdifference > 2:
                 print("Too chaotic")
                 flag = 0
                 break
-            else:
-                for j in range(orderdifference):
-                    print(lineorder)
-                    swap(lineorder, q[pos])
-                    count += 1
+            elif orderdifference == 2:
+                lineorder[pos] = q[pos]
+                lineorder[pos + 1] = temporder[pos]
+                lineorder[pos + 2] = temporder[pos + 1]
+                count += 2
+            elif orderdifference == 1:
+                lineorder[pos] = q[pos]
+                lineorder[pos + 1] = temporder[pos]
+                count += 1
+
     if flag == 1:
         print(f"Final: {lineorder}")
         print(count)
@@ -57,8 +32,8 @@ if __name__ == "__main__":
     # q = [2, 1, 5, 3, 4]
     # print("Correct Answer: 3")
 
-    # q = [2, 5, 1, 3, 4]
-    # print("Correct Answer: Too Chaotic")
+    q = [2, 5, 1, 3, 4]
+    print("Correct Answer: Too Chaotic")
 
     # q = [5, 1, 2, 3, 7, 8, 6, 4]
     # print("Correct Answer: Too Chaotic")
@@ -66,8 +41,8 @@ if __name__ == "__main__":
     # q = [1, 2, 5, 3, 7, 8, 6, 4]
     # print("Correct Answer: 7")
 
-    q = [1, 2, 5, 3, 4, 7, 8, 6]
-    print("Correct Answer: 4")
+    # q = [1, 2, 5, 3, 4, 7, 8, 6]
+    # print("Correct Answer: 4")
 
     # Run program
     minimumbribes(q)
