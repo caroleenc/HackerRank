ec3c1a0079a3876c64ca46af399f97d748c3883e
Caroleen Chen <starkkitt@gmail.com>
2021-11-05 15:42:16 -0400

    recursive attempt

--- New_Year_Chaos.py
+++ New_Year_Chaos.py
@@ -1,4 +1,4 @@
-def minimumbribes(q):
+def jeremy_minimumbribes(q):
     lineorder = list(range(1, len(q)+1))
     count = 0
     flag = 1
@@ -25,24 +25,57 @@ def minimumbribes(q):
         print(f"Final: {lineorder}")
         print(count)
 
+def caroleen_minimumbribes(q):
+    if len(q) == 0 or len(q) == 1:
+        return 0
+    
+    current = 0
+    next = 1
+    total_bribes = 0
+    return compare_values(q, current, next, total_bribes)
+        
+def compare_values(q, current, next, total_bribes):
+    print(current, next)
+    if next == len(q):
+        return total_bribes
+    elif current == next:
+        return compare_values(q, current, next+1, total_bribes)
+    else:
+        print(q[current], q[next], total_bribes)
+        if q[next] - q[current] == 1:
+            print("Sequential", q[current], q[next], total_bribes)
+            return compare_values(q, current+1, next+1, total_bribes+0)
+        elif q[next] - q[current] == -1:
+            print("Partner number", q[current], q[next], total_bribes)
+            return compare_values(q, current+1, next, total_bribes+1)
+        elif q[next] - q[current] < 0:
+            print("Current is greater", q[current], q[next], total_bribes)
+            return compare_values(q, current, next+1, total_bribes+1)
+        elif q[next] - q[current] > 0:
+            print("Current is less", q[current], q[next], total_bribes)
+            return compare_values(q, current+1, next+1, total_bribes+0)
 
 if __name__ == "__main__":
     # Test Cases
+    #q = [1,2,3,4,5,6,7]
+    #q = [3, 1, 2, 5, 4, 6]
 
-    # q = [2, 1, 5, 3, 4]
-    # print("Correct Answer: 3")
+    #q = [2, 1, 5, 3, 4]
+    #print("Correct Answer: 3")
 
-    q = [2, 5, 1, 3, 4]
-    print("Correct Answer: Too Chaotic")
+    #q = [2, 5, 1, 3, 4]
+    #print("Correct Answer: Too Chaotic")
 
     # q = [5, 1, 2, 3, 7, 8, 6, 4]
     # print("Correct Answer: Too Chaotic")
 
-    # q = [1, 2, 5, 3, 7, 8, 6, 4]
+    q = [1, 2, 5, 3, 7, 8, 6, 4]
     # print("Correct Answer: 7")
 
     # q = [1, 2, 5, 3, 4, 7, 8, 6]
     # print("Correct Answer: 4")
 
     # Run program
-    minimumbribes(q)
+    #jeremy_minimumbribes(q)
+
+    print(caroleen_minimumbribes(q))
